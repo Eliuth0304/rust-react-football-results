7eb984af771781e5f9cae47ef0ed0ac14d7f60c3
jack-cooper <jack.cooper@stratumfive.com>
2022-04-19 19:48:57 +0100

    feat(football): Added JSON support for `get_football_data` handler.
    This commit adds full E2E JSON support for the football data handler. Where before the outgoing request was serialising the JSON response as a String, the response is now considered to be a full-fledged JSON object both within the API code and in the response it sends out.
    
    This also has the added benefit of axum automatically adding in the `content-type: application/json` header for us. Neat!

--- src/football/mod.rs
+++ src/football/mod.rs
@@ -1,19 +1,20 @@
+mod results;
+
+pub(crate) use results::FootballResults;
+
 use color_eyre::Report;
 use reqwest::Client;
 
 const FOOTBALL_API: &str = "https://jwcooper.net/football";
 
-#[derive(Clone)]
-pub(crate) struct FootballResults(pub String);
-
-pub(crate) async fn fetch_results(client: &Client) -> Result<String, Report> {
+pub(crate) async fn fetch_results(client: &Client) -> Result<FootballResults, Report> {
     Ok(
         client
             .get(FOOTBALL_API)
             .send()
             .await? // Connection errors
             .error_for_status()? // HTTP errors
-            .text()
+            .json()
             .await?, // Parsing errors
     )
 }
--- src/router/handlers.rs
+++ src/router/handlers.rs
@@ -7,30 +7,24 @@ use axum::{extract::Extension, response::IntoResponse, Json};
 use color_eyre::Report;
 use hyper::{Body, Response};
 use reqwest::Client;
-use serde::Serialize;
 
 pub(crate) async fn get_football_data(
     client: Extension<Client>,
     cached: Extension<Cached<FootballResults>>,
 ) -> Result<impl IntoResponse, ReportError> {
-    #[derive(Serialize)]
-    struct Response {
-        football_results: String,
-    }
-
-    let FootballResults(football_results) = cached
+    let football_results = cached
         .get_cached(|| {
             Box::pin(async move {
                 tracing::info!("Sending an outgoing request!");
 
-                let results = football::fetch_results(&client).await?;
+                let football_results = football::fetch_results(&client).await?;
 
-                Ok::<_, Report>(FootballResults(results))
+                Ok::<_, Report>(football_results)
             })
         })
         .await?;
 
-    Ok(Json(Response { football_results }))
+    Ok(Json(football_results))
 }
 
 #[tracing::instrument]
