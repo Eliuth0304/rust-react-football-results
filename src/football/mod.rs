74ef0006d5ff01147ffffe3882700e8918239e7e
jack-cooper <jack.cooper@stratumfive.com>
2022-04-25 14:27:31 +0100

    feat(football_results): Removed strict JSON tyoe safety as we're operating as a pass-through layer in this API.
    I've elected to simply treat the JSON as a shapeless serde_json::Value here. In addition to simplifying this web server, this means changes beyond my control to the backing API will not cause breakages at this layer, as well as ensuring consistency between the frontend and the backing API, converting this layer to a "real" proxy.

--- src/football/mod.rs
+++ src/football/mod.rs
@@ -1,16 +1,20 @@
-mod results;
-
-pub(crate) use results::FootballResults;
-
 use color_eyre::Report;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-const FOOTBALL_API: &str = "https://jwcooper.net/football";
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub(crate) struct FootballResults(pub Value);
 
 pub(crate) async fn fetch_results(client: &Client) -> Result<FootballResults, Report> {
+    let football_api = match std::env::var("MODE").as_deref() {
+        Ok("release") => "http://jwcooper.net/football",
+        _ => "http://127.0.0.1:8001",
+    };
+
     Ok(
         client
-            .get(FOOTBALL_API)
+            .get(football_api)
             .send()
             .await? // Connection errors
             .error_for_status()? // HTTP errors
