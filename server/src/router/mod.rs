50d27af77f59272465321ebacc057dd0f9a7d644
jack-cooper <jack.cooper@stratumfive.com>
2022-05-05 12:49:27 +0100

    feat(cors): Added blanket CORS support to server
    In the general case I'd be more rigorous than allowing all methods from all origins, but given the highly limited scope of this server this is a passable solution for the time being.

--- server/src/router/mod.rs
+++ server/src/router/mod.rs
@@ -2,9 +2,12 @@ mod handlers;
 
 use crate::{cache::Cached, football::FootballResults};
 use axum::{extract::Extension, routing::get, Router};
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::trace::TraceLayer;
 
 pub(crate) fn create() -> Router {
+    let cors_layer = CorsLayer::new().allow_methods(Any).allow_origin(Any);
+
     Router::new()
         .route("/", get(handlers::hello_world))
         .route("/football", get(handlers::get_football_data))
@@ -12,4 +15,5 @@ pub(crate) fn create() -> Router {
         .layer(Extension(reqwest::Client::new()))
         .layer(Extension(Cached::<FootballResults>::new()))
         .layer(TraceLayer::new_for_http())
+        .layer(cors_layer)
 }
