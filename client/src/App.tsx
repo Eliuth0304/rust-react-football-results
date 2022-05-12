1f4a5e1aea0494dd8cd60499994fbd80dcd3b94b
jack-cooper <jack.cooper@stratumfive.com>
2022-05-12 12:11:17 +0100

    chore(app): Removed counter boilerplate from initial project setup, and replaced main heading with one relevant to this project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import { useGetFootballResultsQuery } from "./services/football";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const { data, error, isLoading } = useGetFootballResultsQuery();
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-thin mt-8 text-center">
-        Hello Vite + React!
-      </h1>
-      <button
-        className="mt-4 w-32 px-4 py-2 rounded bg-blue-400 hover:bg-blue-300 transition-colors"
-        type="button"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Count is: {count}
-      </button>
+      <h1 className="text-4xl font-thin mt-8 text-center">Football Results</h1>
       {data !== undefined && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
