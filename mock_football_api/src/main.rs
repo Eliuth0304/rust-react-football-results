22a85924ebcaef33f6c89d0b34d31a1f71eaa9f4
jack-cooper <jack.cooper@stratumfive.com>
2022-05-12 21:34:05 +0100

    chore(mock_football_api): Moved mock_football_api project up into its own top-level folder in the project to get better IDE autocomplete
    As this was in a sub-folder of another Rust project before VSCode had decided it wasn't included in the module path, despite being its own standalone project.

---
+++ mock_football_api/src/main.rs
@@ -0,0 +1,28 @@
+use axum::{response::Json, routing::get, Router, Server};
+use serde_json::Value;
+use std::{error::Error, net::SocketAddr};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    run_server().await?;
+
+    Ok(())
+}
+
+async fn run_server() -> Result<(), Box<dyn Error>> {
+    let addr: SocketAddr = "0.0.0.0:8001".parse()?;
+
+    let app = Router::new().route("/", get(get_mock_data));
+
+    Server::bind(&addr).serve(app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn get_mock_data() -> Json<Value> {
+    let sample_json = include_str!("sample_response.json");
+
+    let sample_response: Value = serde_json::from_str(sample_json).unwrap();
+
+    Json(sample_response)
+}
