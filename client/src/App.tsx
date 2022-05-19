cd1bcc48b76ed4737e4ac4dcbc9a8c1e73add92c
jack-cooper <jack.cooper@stratumfive.com>
2022-05-19 12:14:05 +0100

    chore(App): Replaced autogenerated function with arrow function for consistency across the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import FootballResults from "./features/football/FootballResults/FootballResults";
 
-function App() {
+const App = () => {
   return (
     <div className="flex flex-col items-center px-2 py-8 font-extralight md:px-32">
       <h1 className="text-4xl font-thin text-center">Football Results</h1>
       <FootballResults />
     </div>
   );
-}
+};
 
 export default App;
