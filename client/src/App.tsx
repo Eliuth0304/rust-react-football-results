b8b8c2bca7577ce151c64e725dcb208e500aae5d
jack-cooper <jack.cooper@stratumfive.com>
2022-05-19 21:19:02 +0100

    feat(App): Added more fine-grained scaling of app padding in the x direction to handle screens of all sizes more gracefully

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import FootballResults from "./features/football/FootballResults/FootballResults
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center px-2 py-8 font-extralight md:px-32">
+    <div className="flex flex-col items-center px-4 py-8 font-extralight sm:px-8 md:px-16 lg:px-32 xl:px-64">
       <h1 className="text-4xl font-thin text-center">Football Results</h1>
       <FootballResults />
     </div>
