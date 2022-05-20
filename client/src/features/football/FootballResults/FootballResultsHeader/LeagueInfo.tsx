cf2c01f4dd578d56d9c26b61241bf0f62a36ebea
jack-cooper <jack.cooper@stratumfive.com>
2022-05-20 14:25:07 +0100

    fix(LeagueInfo): Adjusted font sizes and padding to more gracefully scale across different screen sizes

--- client/src/features/football/FootballResults/FootballResultsHeader/LeagueInfo.tsx
+++ client/src/features/football/FootballResults/FootballResultsHeader/LeagueInfo.tsx
@@ -4,9 +4,9 @@ type Props = {
 };
 
 const LeagueInfo = ({ logoUrl, name }: Props) => (
-  <div className="flex justify-center items-center p-4 border-r border-white grow">
-    <img className="h-8" src={logoUrl} />
-    <p className="ml-4 text-2xl">{name}</p>
+  <div className="flex justify-center items-center px-4 py-2 border-r border-white md:p-4 grow">
+    <img className="h-4 md:h-8" src={logoUrl} />
+    <p className="ml-1 text-lg sm:ml-4 sm:text-2xl">{name}</p>
   </div>
 );
 
