603862ecd9149809ca02d99a3b63d3b0a963ac76
jack-cooper <jack.cooper@stratumfive.com>
2022-05-24 20:42:08 +0100

    fix(MatchFilterTab): Updated button highlight to respect new string literal type values, instead of previous enum string values

--- client/src/features/football/Standings/StandingsHeader/MatchFilterTab.tsx
+++ client/src/features/football/Standings/StandingsHeader/MatchFilterTab.tsx
@@ -8,14 +8,15 @@ type Props = {
 const MatchFilterTab = ({ onClick, text }: Props) => {
   const standingsFilter = useAppSelector((state) => state.standingsFilter);
 
-  const isSelected = text.toLowerCase() === standingsFilter;
+  const isSelected =
+    text.toLowerCase() === standingsFilter ||
+    (text.toLowerCase() === "overall" && standingsFilter === "all");
 
   return (
     <button
       onClick={onClick}
       className={`px-2 py-1 mx-1 my-2 text-xs font-bold uppercase rounded hover:bg-gray-100 ${
-        isSelected ? "bg-gray-100" : ""
-      }`}
+        isSelected ? "bg-gray-100" : ""}`}
     >
       {text}
     </button>
