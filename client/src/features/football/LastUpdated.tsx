fc27dae011154b548cfd694fe627664af50e677c
jack-cooper <jack.cooper@stratumfive.com>
2022-11-10 19:13:51 +0000

    chore(ui): Improved date format for last updated date

--- client/src/features/football/LastUpdated.tsx
+++ client/src/features/football/LastUpdated.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const LastUpdated = ({ date }: Props) => (
   <div className="mt-4 font-thin text-center">
-    <p>Last Updated: {date.toString()}</p>
+    <p>Last Updated: {date.format("DD MMM YYYY")}</p>
   </div>
 );
 
